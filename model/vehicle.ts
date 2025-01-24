8705fdc2fba9b5d58f165847ba01041aaee14337
LRLayan <rameshlayan4@gmail.com>
2025-01-25 01:32:58 +0530

    vehicle init

---
+++ model/vehicle.ts
@@ -0,0 +1,23 @@
+import {Staff} from "./staff";
+
+export class Vehicle {
+    code:string;
+    licensePlateNumber:string
+    vehicleName:string;
+    category:string;
+    fuelType:string;
+    status:string;
+    remark:string;
+    assignStaffMember: Staff;
+
+    constructor(code: string, licensePlateNumber: string, vehicleName: string, category: string, fuelType: string, status: string, remark: string, assignStaffMember: Staff) {
+        this.code = code;
+        this.licensePlateNumber = licensePlateNumber;
+        this.vehicleName = vehicleName;
+        this.category = category;
+        this.fuelType = fuelType;
+        this.status = status;
+        this.remark = remark;
+        this.assignStaffMember = assignStaffMember;
+    }
+}
