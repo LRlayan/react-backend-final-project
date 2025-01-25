99c7db4f58c43f55ea4022308bb35cc5b82e4063
LRLayan <rameshlayan4@gmail.com>
2025-01-26 02:06:05 +0530

    apply mongoose and mongodb

--- server.ts
+++ server.ts
@@ -1,21 +1,31 @@
 import express from 'express';
+import vehicleRoutes from "./routes/vehicle-routes";
+import mongoose from "mongoose";
 
 const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 app.use(cors({
     origin: "http://localhost:5173",
     method: ['GET','POST','PUT','PATCH','DELETE'],
     allowedHeaders: ['Content-Type','Authorization']
 }));
 
-app.use('/crop');
-app.use('/field');
-app.use('/log');
-app.use('/staff');
-app.use('/equipment');
-app.use('/vehicle');
+mongoose.connect("mongodb://localhost:27017/cropMonitoringDB")
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch(err => {
+        console.error("Failed to connect to MongoDB", err);
+    });
+
+
+// app.use('/crop');
+// app.use('/field');
+// app.use('/log');
+// app.use('/staff');
+// app.use('/equipment');
+app.use('/vehicle',vehicleRoutes);
 
 app.listen(3000, () => console.log("Server start 3000 port"));
