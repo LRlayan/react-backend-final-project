a20018d378b7d90ebca00095730998c7e7b2135e
LRLayan <rameshlayan4@gmail.com>
2025-01-26 02:05:48 +0530

    saveVehicle

--- routes/vehicle-routes.ts
+++ routes/vehicle-routes.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import {saveVehicle} from "../database/mongoose-vehicle-data-store";
 
 const vehicleRoutes = express.Router();
 
-vehicleRoutes.post('/saveVehicle', async (req,res) => {
-
+vehicleRoutes.post('/saveVehicle', async (req, res) => {
+    const vehicle = req.body;
+    try {
+        await saveVehicle(vehicle);
+        res.status(200).send("Vehicle saved successfully.");
+    } catch (e) {
+        console.error("Failed to save vehicle!", e);
+        res.status(400).send("Failed to save vehicle. Please try again.");
+    }
 });
 
 vehicleRoutes.put('/updateVehicle/:vehicleId', async (req,res) => {
