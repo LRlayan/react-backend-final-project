489e3341a7e7b491f763b7528bcbdf3abb2c9040
LRLayan <rameshlayan4@gmail.com>
2025-01-27 00:19:34 +0530

    staff.ts

--- model/staff.ts
+++ model/staff.ts
@@ -1,12 +1,16 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
+export type RoleType = 'ADMINISTRATIVE' | 'MANAGER' | 'SCIENTIFIC' | 'OTHER';
+export type DesignationType = "ASSISTANT MANAGER" | "ADMIN AND HR STAFF" | "OFFICE ASSISTANT" | "SENIOR AGRONOMIST" | "AGRONOMIST" | "SENIOR TECHNICIAN" | "TECHNICIAN" | "SUPERVISOR" | "LABOUR"
+export type GenderType = "Male" | "Female";
+
 export interface IStaff extends Document {
     code: string;
     firstName: string;
     lastName: string;
     joinedDate: string;
-    designation: string;
-    gender: string;
+    designation: DesignationType;
+    gender: GenderType;
     dob: string;
     addressLine01: string;
     addressLine02: string;
@@ -15,17 +19,17 @@ export interface IStaff extends Document {
     addressLine05: string;
     mobile: string;
     email: string;
-    role: string;
-    assignVehicles: mongoose.Types.ObjectId[];
+    role: RoleType;
+    assignVehicles?: mongoose.Types.ObjectId[];
 }
 
 const staffSchema = new Schema<IStaff>({
-    code: { type: String, required: true },
+    code: { type: String, required: true, unique: true},
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     joinedDate: { type: String, required: true },
     designation: { type: String, required: true, enum: ["ASSISTANT MANAGER","ADMIN AND HR STAFF","OFFICE ASSISTANT","SENIOR AGRONOMIST","AGRONOMIST","AGRONOMIST","SENIOR TECHNICIAN","TECHNICIAN","SUPERVISOR","LABOUR"]},
-    gender: { type: String, required: true, enum: ['Male','Female'] },
+    gender: { type: String, required: true, enum: ["Male","Female"]},
     dob: { type: String, required: true },
     addressLine01: { type: String, required: true },
     addressLine02: { type: String, required: false },
@@ -33,7 +37,7 @@ const staffSchema = new Schema<IStaff>({
     addressLine04: { type: String, required: false },
     addressLine05: { type: String, required: false },
     mobile: { type: String, required: true },
-    email: { type: String, required: true, match:/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/},
+    email: { type: String, required: true, unique: true},
     role: { type: String, required: true, enum: ['ADMINISTRATIVE', 'MANAGER', 'SCIENTIFIC','OTHER']},
     assignVehicles: [
         { type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle' }
