041b1525a90216d90ef326924bff251500809679
LRLayan <rameshlayan4@gmail.com>
2025-01-27 00:22:49 +0530

    saveStaff

--- routes/staff-routes.ts
+++ routes/staff-routes.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import {saveStaff} from "../database/staff-data-store";
 
 const staffRoutes = express.Router();
 
 staffRoutes.post('/saveStaff', async (req,res) => {
-
+    const staff = req.body;
+    console.log("Staff - : ", staff);
+    try {
+        await saveStaff(staff);
+        res.status(200).send("staff saved successfully.");
+    } catch (e) {
+        console.error("Failed to save staff!", e);
+        res.status(400).send("Failed to save staff. Please try again.");
+    }
 });
 
 staffRoutes.put('/updateStaff', async (req,res) => {
