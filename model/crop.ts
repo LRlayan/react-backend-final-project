3120411b35c8f86fefca9cbc9344e2380ca73ce1
LRLayan <rameshlayan4@gmail.com>
2025-01-28 16:13:54 +0530

    relation to logs and field

--- model/crop.ts
+++ model/crop.ts
@@ -1,26 +1,26 @@
-import {Field} from "./field";
-import {Log} from "./log";
+import mongoose, {Schema} from "mongoose";
 
-export class Crop {
-    id:number;
+export interface ICrop {
     code:string;
     name:string;
     scientificName:string;
     category:string;
     season:string;
-    image: File | null;
-    assignFields: Field[];
-    assignLogs: Log[];
-
-    constructor(id:number, code: string, name: string, scientificName: string, category: string, season: string, image: File | null, assignFields: Field[], assignLogs: Log[]) {
-        this.id = id;
-        this.code = code;
-        this.name = name;
-        this.scientificName = scientificName;
-        this.category = category;
-        this.season = season;
-        this.image = image;
-        this.assignFields = assignFields;
-        this.assignLogs = assignLogs;
-    }
+    image: Buffer | string | null;
+    assignFields: mongoose.Types.ObjectId[];
+    assignLogs:mongoose.Types.ObjectId[];
 }
+
+const cropSchema = new Schema<ICrop>({
+    code: { type: String, required: true, unique: true},
+    name: { type: String, required: true},
+    scientificName: { type: String, required: true},
+    category: { type: String, required: true},
+    season: { type: String, required: true},
+    image: { type: Buffer | String, required: true},
+    assignFields: [{ type: mongoose.Schema.Types.ObjectId, ref: "Field"}],
+    assignLogs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Log"}]
+});
+
+const Crop = mongoose.model<any>("Crop", cropSchema);
+export default Crop;
