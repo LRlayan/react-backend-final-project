dafde3d0fd3b88519b327d5d4cdb2039ddfbc4e3
LRLayan <rameshlayan4@gmail.com>
2025-01-28 16:56:07 +0530

    define enum

--- model/crop.ts
+++ model/crop.ts
@@ -1,11 +1,13 @@
 import mongoose, {Schema} from "mongoose";
 
+export type SeasonType = "Yala Season" | "Maha Season";
+
 export interface ICrop {
     code:string;
     name:string;
     scientificName:string;
     category:string;
-    season:string;
+    season:SeasonType;
     image: Buffer | string | null;
     assignFields: mongoose.Types.ObjectId[];
     assignLogs:mongoose.Types.ObjectId[];
@@ -16,7 +18,7 @@ const cropSchema = new Schema<ICrop>({
     name: { type: String, required: true},
     scientificName: { type: String, required: true},
     category: { type: String, required: true},
-    season: { type: String, required: true},
+    season: { type: String, required: true, enum: ["Yala Season", "Maha Season"]},
     image: { type: Buffer | String, required: true},
     assignFields: [{ type: mongoose.Schema.Types.ObjectId, ref: "Field"}],
     assignLogs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Log"}]
