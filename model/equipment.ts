d9a68372aaa03e3ea0605b8cada42541391c1622
LRLayan <rameshlayan4@gmail.com>
2025-01-29 00:46:30 +0530

    remove unused

--- model/equipment.ts
+++ model/equipment.ts
@@ -1,5 +1,4 @@
 import mongoose, {Schema} from "mongoose";
-import {StatusType} from "./vehicle";
 
 export type StatusType = "Available" | "Unavailable";
 export type EquipmentType = "Hand Tools" | "Irrigation Equipment" | "Power Tools and Machinery" | "Ploughing Equipment" | "Weeding and Pest Control Equipment" | "Harvesting Equipment" | "Post-Harvest Equipment" | "Monitoring and Measuring Tools" | "Protective Equipment";
