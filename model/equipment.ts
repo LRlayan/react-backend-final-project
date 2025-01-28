7beaac02f86961773fbef16fd8d930037556234f
LRLayan <rameshlayan4@gmail.com>
2025-01-28 16:52:22 +0530

    define enum

--- model/equipment.ts
+++ model/equipment.ts
@@ -2,11 +2,12 @@ import mongoose, {Schema} from "mongoose";
 import {StatusType} from "./vehicle";
 
 export type StatusType = "Available" | "Unavailable";
+export type EquipmentType = "Hand Tools" | "Irrigation Equipment" | "Power Tools and Machinery" | "Ploughing Equipment" | "Weeding and Pest Control Equipment" | "Harvesting Equipment" | "Post-Harvest Equipment" | "Monitoring and Measuring Tools" | "Protective Equipment";
 
 export interface IEquipment {
     code:string;
     name:string;
-    equType:string;
+    equType:EquipmentType;
     status:StatusType;
     count:number;
     assignStaffMembers: mongoose.Types.ObjectId[];
@@ -16,7 +17,7 @@ export interface IEquipment {
 const equipmentSchema = new Schema<IEquipment>({
     code: { type: String, required: true, unique: true},
     name: { type: String, required: true},
-    equType: { type: String, required: true},
+    equType: { type: String, required: true, enum: ["Hand Tools","Irrigation Equipment","Power Tools and Machinery","Ploughing Equipment","Weeding and Pest Control Equipment","Harvesting Equipment","Post-Harvest Equipment","Monitoring and Measuring Tools","Protective Equipment"]},
     status: { type: String, required: true, enum: ["Available", "Unavailable"]},
     count: { type: Number, required: true},
     assignStaffMembers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Staff"}],
