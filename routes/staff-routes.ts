b19352f5128920730fcd0644766288faf5c890e1
LRLayan <rameshlayan4@gmail.com>
2025-01-31 01:52:24 +0530

    data validate using staff model

--- routes/staff-routes.ts
+++ routes/staff-routes.ts
@@ -1,14 +1,19 @@
 import express from "express";
-import {saveStaff} from "../repository/staff-repository";
+import {saveStaffService} from "../service/staff-service";
+import {StaffModel} from "../models/staff-model";
 
 const staffRoutes = express.Router();
 
 staffRoutes.post('/saveStaff', async (req,res) => {
     const staff = req.body;
-    console.log("Staff - : ", staff);
     try {
-        await saveStaff(staff);
-        res.status(200).send("staff saved successfully.");
+        const newStaff = new StaffModel(staff.code, staff.firstName, staff.lastName, staff.joinedDate, staff.designation, staff.gender, staff.dob, staff.addressLine01, staff.addressLine02, staff.addressLine03, staff.addressLine04, staff.addressLine05, staff.mobile, staff.email, staff.role, staff.assignVehicles, staff.assignLogs, staff.assignFields, staff.assignEquipments);
+        if (newStaff) {
+            const result = await saveStaffService(newStaff);
+            res.status(200).send(result);
+        } else {
+            console.log("Error, Required staff data!");
+        }
     } catch (e) {
         console.error("Failed to save staff!", e);
         res.status(400).send("Failed to save staff. Please try again.");
