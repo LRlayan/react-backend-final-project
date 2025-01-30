8ff552419e76f3eb5c4246a6004ec6429cc051d0
LRLayan <rameshlayan4@gmail.com>
2025-01-31 03:40:33 +0530

    added ?

--- models/equipment-model.ts
+++ models/equipment-model.ts
@@ -1,14 +1,11 @@
-import mongoose from "mongoose";
-import {EquipmentType, StatusType} from "../schema/equipment-schema";
-
 export class EquipmentModel {
     code:string;
     name:string;
     equType:string;
     status:string;
     count:number;
-    assignStaffMembers: string[];
-    assignFields: string[];
+    assignStaffMembers?: string[];
+    assignFields?: string[];
 
     constructor(code: string, name: string, equType: string, status: string, count: number, assignStaffMembers: string[], assignFields: string[]) {
         this.code = code;
