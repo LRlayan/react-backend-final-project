c519c59bd72c4d0f94a6cddd424f2e392d7e6e1b
LRLayan <rameshlayan4@gmail.com>
2025-01-31 03:41:01 +0530

    saveEquipment()

--- routes/equipment-routes.ts
+++ routes/equipment-routes.ts
@@ -1,20 +1,34 @@
 import express from "express";
+import {saveEquipmentService} from "../service/equipment-service";
+import {EquipmentModel} from "../models/equipment-model";
 
 const equipmentRoutes = express.Router();
 
-equipmentRoutes.post('saveEquipment', async (req,res) => {
+equipmentRoutes.post('/saveEquipment', async (req,res) => {
+    const equipment = req.body;
+    try {
+        const newEquipment = new EquipmentModel(equipment.code, equipment.name, equipment.equType, equipment.status, equipment.count, equipment.assignStaffMembers, equipment.assignFields);
+        if (newEquipment) {
+            const result = await saveEquipmentService(newEquipment);
+            res.status(201).send(result);
+        } else {
+            console.log("Error, Required equipment data!");
+        }
+    } catch (e) {
+        console.log("Failed to save equipment!",e);
+        res.status(400).send("Failed to save equipment. Please try again.");
+    }
+});
+
+equipmentRoutes.put('/updateEquipment/:equId', async (req,res) => {
 
 });
 
-equipmentRoutes.put('updateEquipment/:equId', async (req,res) => {
-
-});
-
-equipmentRoutes.delete('deleteEquipment/:equId', async (req,res) => {
+equipmentRoutes.delete('/deleteEquipment/:equId', async (req,res) => {
 
 })
 
-equipmentRoutes.get('getAllEquipment', async (req,res) => {
+equipmentRoutes.get('/getAllEquipment', async (req,res) => {
 
 });
 
