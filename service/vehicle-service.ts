4dcdf209cc85c9b465f888f4644c1e7a2f2932f4
LRLayan <rameshlayan4@gmail.com>
2025-02-02 15:13:12 +0530

    updateVehicleService()

--- service/vehicle-service.ts
+++ service/vehicle-service.ts
@@ -1,5 +1,8 @@
 import {VehicleModel} from "../models/vehicle-model";
-import {saveVehicle} from "../repository/vehicle-repository";
+import {findVehicleByCode, saveVehicle, updateVehicle} from "../repository/vehicle-repository";
+import {StatusType} from "../schema/vehicle";
+import mongoose from "mongoose";
+import Staff from "../schema/staff";
 
 export async function saveVehicleService(vehicleData: VehicleModel) {
     try {
@@ -8,5 +11,35 @@ export async function saveVehicleService(vehicleData: VehicleModel) {
     } catch (e) {
         console.error("Service layer error: Failed to save vehicle!");
         throw new Error("Failed to save vehicle, Please try again.");
+    }
+}
+
+export async function updateVehicleService(vehicleData: VehicleModel) {
+    try {
+        const existingVehicle = await findVehicleByCode(vehicleData.vehicleCode);
+        if (!existingVehicle) {
+            throw new Error("Vehicle not found!");
+        }
+
+        // Convert staff codes to ObjectIds
+        let updatedStaffIds: mongoose.Types.ObjectId[] = [];
+        if (vehicleData.assignStaff && Array.isArray(vehicleData.assignStaff)) {
+            const staffDocs = await Staff.find({ code: { $in: vehicleData.assignStaff } });
+            updatedStaffIds = staffDocs.map(staff => staff._id as mongoose.Types.ObjectId);
+        }
+
+        existingVehicle.licensePlateNumber = vehicleData.licensePlateNumber;
+        existingVehicle.vehicleName = vehicleData.vehicleName;
+        existingVehicle.category = vehicleData.category;
+        existingVehicle.fuelType = vehicleData.fuelType;
+        existingVehicle.status = vehicleData.status as StatusType;
+        existingVehicle.remark = vehicleData.remark;
+        existingVehicle.assignStaff = updatedStaffIds;
+
+        const updatedVehicle = await updateVehicle(existingVehicle);
+        return updatedVehicle;
+    } catch (e) {
+        console.error("Service layer error: Failed to update vehicle!", e);
+        throw new Error("Failed to update vehicle, Please try again.");
     }
 }
