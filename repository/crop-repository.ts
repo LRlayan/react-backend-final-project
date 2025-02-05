dea154d04bf2653c24ece3f80e98888ae42325b5
LRLayan <rameshlayan4@gmail.com>
2025-02-06 01:08:34 +0530

    updateCropAssignLog()

--- repository/crop-repository.ts
+++ repository/crop-repository.ts
@@ -1,4 +1,7 @@
 import Crop, {ICrop} from "../schema/crop";
+import {LogModel} from "../models/log-model";
+import Log from "../schema/log";
+import mongoose from "mongoose";
 
 interface Crop {
     code: string;
@@ -39,6 +42,33 @@ export async function updateCrop(code: string, updateData: Partial<ICrop>) {
             : {message:"Crop update Unsuccessfully"}
     }catch (e) {
         console.error("Failed to update crop:", e);
+        throw e;
+    }
+}
+
+export async function updateCropAssignLog(code: string, logData: LogModel) {
+    try {
+        const logDocs = await Log.findOne({ code }).lean<{ _id: mongoose.Types.ObjectId } | null>();
+        if (!logDocs) {
+            throw new Error(`Log with code ${code} not found`);
+        }
+        const logId = logDocs._id;
+
+        let cropCodes: mongoose.Types.ObjectId[] = [];
+        const cropDocs = await Crop.find({ code: { $in: logData.assignCrops }}).lean<{ _id: mongoose.Types.ObjectId }[]>();
+        cropCodes = cropDocs.map((crop) => crop._id);
+
+        await Crop.updateMany(
+            { assignLogs: logId },
+            { $pull: logId }
+        );
+
+        await Crop.updateMany(
+            { _id: { $in: cropCodes }},
+            { $addToSet: { assignLogs: logId }}
+        );
+        return cropCodes;
+    } catch (e) {
         throw e;
     }
 }
