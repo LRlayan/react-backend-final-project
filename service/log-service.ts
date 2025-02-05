2fdbf0b9bfe85424970ad11d8fc92edf77a789d0
LRLayan <rameshlayan4@gmail.com>
2025-02-06 01:10:55 +0530

    updateLogService()

--- service/log-service.ts
+++ service/log-service.ts
@@ -1,10 +1,13 @@
 import {LogModel} from "../models/log-model";
-import {saveLog} from "../repository/log-repository";
+import {findLogById, saveLog, updateLog} from "../repository/log-repository";
 import mongoose from "mongoose";
 import Field from "../schema/field";
 import Staff from "../schema/staff";
 import Crop from "../schema/crop";
-import Log from "../schema/log";
+import Log, {ILog} from "../schema/log";
+import {updateCropAssignLog} from "../repository/crop-repository";
+import {updateStaffAssignLog} from "../repository/staff-repository";
+import {updateFieldAssignLog} from "../repository/field-repository";
 
 export async function saveLogService(logData: LogModel) {
     try {
@@ -37,5 +40,45 @@ export async function saveLogService(logData: LogModel) {
     } catch (e) {
         console.error("Service layer error: Failed to save logs!");
         throw new Error("Failed to save logs. Please try again.");
+    }
+}
+
+export async function updateLogService(logData: LogModel) {
+    try {
+        const excitingLog = await findLogById(logData.code);
+        if (!excitingLog) {
+            throw new Error("Log not found");
+        }
+
+        let updatedFieldIds : mongoose.Types.ObjectId[] = [];
+        let updatedStaffIds : mongoose.Types.ObjectId[] = [];
+        let updatedCropIds : mongoose.Types.ObjectId[] = [];
+        if (logData.assignFields && Array.isArray(logData.assignFields) && logData.assignStaff && Array.isArray(logData.assignStaff) && logData.assignCrops && Array.isArray(logData.assignCrops)) {
+            const fieldDocs = await Field.find({ code: { $in: logData.assignFields }});
+            updatedFieldIds = fieldDocs.map((field) => field._id as mongoose.Types.ObjectId);
+
+            const staffDocs = await Staff.find({ code: { $in: logData.assignStaff }});
+            updatedStaffIds = staffDocs.map((staff) => staff._id as mongoose.Types.ObjectId);
+
+            const cropDocs = await Crop.find({ code: { $in: logData.assignCrops }});
+            updatedCropIds = cropDocs.map((crop) => crop._id as mongoose.Types.ObjectId);
+        }
+
+        const updatedData : Partial<ILog> = {
+            name: logData.name,
+            logDate: logData.logDate,
+            logDetails: logData.logDetails,
+            image: logData.image,
+            assignFields: updatedFieldIds,
+            assignStaff: updatedStaffIds,
+            assignCrops: updatedCropIds
+        }
+
+        const updatedLogAssignFields = await updateFieldAssignLog(logData.code, logData);
+        const updatedLogAssignStaff = await updateStaffAssignLog(logData.code, logData);
+        const updatedLogAssignCrop = await updateCropAssignLog(logData.code, logData);
+        return await updateLog(logData.code,updatedData);
+    } catch (e) {
+
     }
 }
