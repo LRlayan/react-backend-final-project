6dea89189e7061802ae6bc24c53cae22aaa8a216
LRLayan <rameshlayan4@gmail.com>
2025-02-14 16:56:05 +0530

    get codes and sets

--- routes/staff-routes.ts
+++ routes/staff-routes.ts
@@ -9,11 +9,21 @@ staffRoutes.post('/saveStaff', async (req,res) => {
     const staff = req.body;
     try {
         const idGenerator = new IdGenerator();
+        const parsedAssignVehicle: string[] = staff.assignVehicles ? staff.assignVehicles : [];
+        const parsedAssignLog: string[] = staff.assignLogs ? staff.assignLogs : [];
+        const parsedAssignField: string[] = staff.assignFields ? staff.assignFields : [];
+        const parsedAssignEquipment: string[] = staff.assignEquipments ? staff.assignEquipments : [];
+
+        const vehicleCodes = parsedAssignVehicle.map((vehicle: any) => vehicle.vehicleCode);
+        const logCodes = parsedAssignLog.map((log: any) => log.code);
+        const fieldCodes = parsedAssignField.map((field: any) => field.code);
+        const equipmentCodes = parsedAssignEquipment.map((equ: any) => equ.code);
+
         const newCode = await idGenerator.generateId('STAFF-');
         if (newCode === null) {
             throw new Error("staff code is null. Please check the Id Type!");
         }
-        const newStaff = new StaffModel(newCode, staff.firstName, staff.lastName, staff.joinedDate, staff.designation, staff.gender, staff.dob, staff.addressLine01, staff.addressLine02, staff.addressLine03, staff.addressLine04, staff.addressLine05, staff.mobile, staff.email, staff.role, staff.assignVehicles, staff.assignLogs, staff.assignFields, staff.assignEquipments);
+        const newStaff = new StaffModel(newCode, staff.firstName, staff.lastName, staff.joinedDate, staff.designation, staff.gender, staff.dob, staff.addressLine01, staff.addressLine02, staff.addressLine03, staff.addressLine04, staff.addressLine05, staff.mobile, staff.email, staff.role, vehicleCodes, logCodes, fieldCodes, equipmentCodes);
         if (newStaff) {
             const result = await saveStaffService(newStaff);
             res.status(201).send(result);
@@ -30,7 +40,17 @@ staffRoutes.put('/updateStaff/:code', async (req,res) => {
     const code = req.body.code;
     const staff = req.body;
     try {
-        const updateStaffMember = new StaffModel(code,staff.firstName, staff.lastName, staff.joinedDate, staff.designation, staff.gender, staff.dob, staff.addressLine01, staff.addressLine02, staff.addressLine03, staff.addressLine04, staff.addressLine05, staff.mobile, staff.email, staff.role, staff.assignVehicles, staff.assignLogs, staff.assignFields, staff.assignEquipments);
+        const parsedAssignVehicle: string[] = staff.assignVehicles ? staff.assignVehicles : [];
+        const parsedAssignLog: string[] = staff.assignLogs ? staff.assignLogs : [];
+        const parsedAssignField: string[] = staff.assignFields ? staff.assignFields : [];
+        const parsedAssignEquipment: string[] = staff.assignEquipments ? staff.assignEquipments : [];
+
+        const vehicleCodes = parsedAssignVehicle.map((vehicle: any) => vehicle.vehicleCode);
+        const logCodes = parsedAssignLog.map((log: any) => log.code);
+        const fieldCodes = parsedAssignField.map((field: any) => field.code);
+        const equipmentCodes = parsedAssignEquipment.map((equ: any) => equ.code);
+
+        const updateStaffMember = new StaffModel(code,staff.firstName, staff.lastName, staff.joinedDate, staff.designation, staff.gender, staff.dob, staff.addressLine01, staff.addressLine02, staff.addressLine03, staff.addressLine04, staff.addressLine05, staff.mobile, staff.email, staff.role, vehicleCodes, logCodes, fieldCodes, equipmentCodes);
         if (updateStaffMember) {
             const result = await updateStaffService(updateStaffMember);
             res.status(200).send(result);
