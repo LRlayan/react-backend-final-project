fc3fbb6cf4193f5f0f070c84a9da9709345e3bbb
LRLayan <rameshlayan4@gmail.com>
2025-02-16 01:33:55 +0530

    refactor log model

--- models/log-model.ts
+++ models/log-model.ts
@@ -6,9 +6,9 @@ export class LogModel {
     logDate: string;
     logDetails: string;
     image: string | null;
-    assignFields?: string[];
-    assignStaff?: string[];
-    assignCrops?: string[];
+    assignFields: string[];
+    assignStaff: string[];
+    assignCrops: string[];
 
     constructor(code: string, name: string, logDate: string, logDetails: string, image: string | null, assignFields: string[], assignStaff: string[], assignCrops: string[]) {
         this.code = code;
