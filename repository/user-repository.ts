84810c826f8fe4108e24437b053a0476e57bc1b1
LRLayan <rameshlayan4@gmail.com>
2025-02-18 21:21:33 +0530

    saveUser()

--- repository/user-repository.ts
+++ repository/user-repository.ts
@@ -1,6 +1,30 @@
 import User from "../schema/user";
 import bcrypt from 'bcrypt';
 
+interface User {
+    username: string;
+    email: string;
+    password: string;
+}
+
+export async function saveUser(user: User) {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    try {
+        user.password = hashedPassword;
+        const newUser = new User(user);
+        const result = await newUser.save();
+        if (result) {
+            return result;
+        } else {
+            return { message: "Failed to save user. Please try again."}
+            throw new Error("Failed to save user. Please try again.");
+        }
+    } catch (e) {
+        console.error("Failed to save user:", e);
+        throw e;
+    }
+}
+
 export async function verifyUserCredentials(user: User) {
     const findUser: User | null = await User.findOne({username: user.username});
     if (!user) {
