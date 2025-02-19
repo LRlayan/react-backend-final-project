b87732f42ea86dc40a0e3a28f2c317095ac36f44
LRLayan <rameshlayan4@gmail.com>
2025-02-20 03:02:32 +0530

    verifyUserCredentialsService

--- service/user-service.ts
+++ service/user-service.ts
@@ -1,10 +1,21 @@
 import {UserModel} from "../models/user-model";
-import {saveUser} from "../repository/user-repository";
+import {saveUser, verifyUserCredentials} from "../repository/user-repository";
 
 export async function saveUserService(user: UserModel) {
     try {
         const users = new UserModel(user.username, user.email, user.password);
         return await saveUser(users);
+    } catch (e) {
+        throw e;
+    }
+}
+
+export async function verifyUserCredentialsService(username: string, password: string) {
+    try {
+        if (!username && !password) {
+            console.error(`Please required username: ${username} and password: ${password}`);
+        }
+        return verifyUserCredentials(username, password);
     } catch (e) {
         throw e;
     }
